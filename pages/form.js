47202c2d40d25fdc5f97c7128d9f184b16f2d12d
TTT <tttangcuhk@gmail.com>
2022-05-09 23:47:56 +0800

    Updated Login page as default layout.

--- pages/form.js
+++ pages/form.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import Link from 'next/link';
+import Login from "./forms/login";
 
 const FormContainer = ({children}) => {
 
-    return (
-        <div className="grid h-screen place-item-center bg-[url('../public/ss-scaled-2048x1152.jpg')]">
-            <React.Fragment>
-                <div className="bg-white box-border drop-shadow-xl w-96 p-4 border-4 rounded-2xl content-center place-self-center">
-                    <div className="underline underline-offset-4 decoration-2 m-1 place-item-center">
-                        <Link href="/forms/login">
-                            <a className="font-bold mx-2">➯ Sign In!</a>
-                        </Link>
-                        <Link href="/forms/signup">
-                            <a className="font-bold mx-2">🖉 Sign Up!</a>
-                        </Link>
+    if (children == null) {
+        return (
+            <div className="grid h-screen place-item-center bg-[url('../public/ss-scaled-2048x1152.jpg')]">
+                <React.Fragment>
+                    <Login/>
+                </React.Fragment>
+            </div>
+        );
+    }
+
+    else {
+        return (
+            <div className="grid h-screen place-item-center bg-[url('../public/ss-scaled-2048x1152.jpg')]">
+                <React.Fragment>
+                    <div className="bg-white box-border drop-shadow-xl w-96 p-4 border-4 rounded-2xl content-center place-self-center">
+                        <div className="underline underline-offset-4 decoration-2 m-1 place-item-center">
+                            <Link href="/forms/login">
+                                <a className="font-bold mx-2">➯ Sign In!</a>
+                            </Link>
+                            <Link href="/forms/signup">
+                                <a className="font-bold mx-2">🖉 Sign Up!</a>
+                            </Link>
+                        </div>
+                        {children}
                     </div>
-                    {children}
-                </div>
-            </React.Fragment>
-        </div>
-    );
+                </React.Fragment>
+            </div>
+        );
+    }
+
 }
 
 export default FormContainer
