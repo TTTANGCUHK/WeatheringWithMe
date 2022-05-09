4f81b1d1bb14dcb1901d05b1874af6b483ed9c30
MelodyHoKY <100144828+MelodyHoKY@users.noreply.github.com>
2022-05-10 05:35:30 +0800

    Brief admin layout

---
+++ pages/admin.jsx
@@ -0,0 +1,321 @@
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+import { Collapse } from "react-collapse";
+
+
+class AdminHome extends React.Component {
+  
+  
+  render() {
+    return (
+    
+
+        <div className="w-screen h-full min-h-screen bg-fixed" style={{backgroundImage: "url('../ss-scaled-2048x1152.jpg')"}}>
+        
+        
+        <div className="flex flex-row h-full min-h-screen opacity-90">
+        <div className="flex-none basis-3/12 bg-white rounded m-5 h-fit sticky top-0">
+            <div className="grid grid-rows-4 gap-3 m-3 h-full font-overpass font-bold text-md">
+            <div className="rounded bg-gradient-to-r from-indigo-500 to-cyan-300 hover:from-purple-500 hover:to-rose-500 w-full p-0.5">
+                <a className="rounded w-full bg-white p-2 text-slate-600 h-full text-center" type="button" href="#req">Request Updated Data</a>
+            </div>
+
+            <div className="rounded bg-gradient-to-r from-indigo-500 to-cyan-300 hover:from-purple-500 hover:to-rose-500 w-full p-0.5">
+                <a className="rounded w-full bg-white p-2 text-slate-600 h-full text-center" type="button" href="#loc">Location Data</a>
+            </div>
+
+            <div className="rounded bg-gradient-to-r from-indigo-500 to-cyan-300 hover:from-purple-500 hover:to-rose-500 w-full p-0.5">
+                <a className="rounded w-full bg-white p-2 text-slate-600 h-full text-center" type="button" href="#user">User Data</a>
+            </div>
+
+            <div className="rounded bg-gradient-to-r from-indigo-500 to-cyan-300 hover:from-purple-500 hover:to-rose-500 w-full p-0.5">
+                <a className="rounded w-full bg-white p-2 text-slate-600 h-full text-center" type="button">Log Out</a>
+            </div>
+
+            </div>
+
+
+            
+        </div>
+        <div className="flex-auto bg-white rounded mr-5 mt-5 mb-5">
+            <Request/>
+            <LocationData/>
+            <UserData/>
+        
+        </div>
+
+   
+
+        </div>
+
+        </div>
+    
+    );
+    }
+    }
+
+    class Request extends React.Component {
+        state = {
+            collapseOpen: false,
+          };
+
+        onCollapse = () => {
+            this.setState(prevState => ({
+              collapseOpen: !prevState.collapseOpen,
+            }));
+        };
+        render(){
+            return(
+                <div className="m-3" id="req">
+                    <div className="rounded bg-gradient-to-r to-indigo-500 from-cyan-300 hover:from-purple-500 hover:to-rose-500 w-full p-0.5 opacity-80 font-overpass text-xl font-bold">
+                    <div className="rounded w-full bg-white p-2 h-full opacity-80 text-center" onClick={this.onCollapse}>Request Updated Data</div>
+                    </div>
+
+                    <Collapse isOpened={this.state.collapseOpen}>
+                    <div className="rounded w-full p-2 border-2 border-indigo-100 mt-2 mb-5">
+                    <button className="bg-sky-600 hover:bg-sky-400 rounded p-1 font-overpass text-white px-2" type="button">Reload Data</button>
+                    <div className="text-xs font-overpass mt-0.5">Last updated: </div>
+                    </div>
+                    </Collapse>
+
+
+                    
+                </div>
+            );
+        }
+    }
+
+    class LocationData extends React.Component {
+        state = {
+            collapseOpen: false,
+          };
+
+        onCollapse = () => {
+            this.setState(prevState => ({
+              collapseOpen: !prevState.collapseOpen,
+            }));
+        };
+        render(){
+            return(
+                <div className="m-3" id="loc">
+                    <div className="rounded bg-gradient-to-r to-indigo-500 from-cyan-300 hover:from-purple-500 hover:to-rose-500 w-full p-0.5 opacity-80 font-overpass text-xl font-bold">
+                    <div className="rounded w-full bg-white p-2 h-full opacity-80 text-center" onClick={this.onCollapse}>Location Data</div>
+                    </div>
+
+                    <Collapse isOpened={this.state.collapseOpen}>
+                    <div className="rounded w-full p-2 border-2 border-indigo-100 mt-2 mb-5">
+
+                    <div className="columns-4 mb-3 mx-1 mt-1">                        
+
+                    <div className="rounded-t w-24 bg-sky-600 text-white text-center font-overpass p-1 px-2">CREATE</div>
+                    <div className="border rounded-b p-2 font-overpass">
+                        <form>
+                        <label className="block text-sm font-bold" name="name">Name</label>
+                        <input className="w-full py-1 border rounded px-1" id="name" type="text" />
+
+                        <label className="block text-sm mt-2 font-bold" name="latitude">Latitude</label>
+                        <input className="w-full py-1 border rounded px-1" id="latitude" type="text" />
+
+                        <label className="block text-sm mt-2 font-bold" name="longtitude">Longtitude</label>
+                        <input className="w-full py-1 border rounded px-1" id="longtitude" type="text" />
+
+                        <button className="rounded bg-sky-600 hover:bg-sky-400 py-1 px-5 text-white text-center font-overpass mt-2" type="submit">CREATE</button>
+                        </form>
+                    </div>
+
+                    <div className="rounded-t w-24 bg-sky-600 text-white text-center font-overpass p-1 px-2 mt-3">UPDATE</div>
+                    <div className="border rounded-b p-2 font-overpass">
+                        <form>
+                        <label className="block text-sm font-bold" name="name">Name</label>
+                        <input className="w-full py-1 border rounded px-1" id="name" type="text" />
+
+                        <label className="block text-sm mt-2 font-bold" name="latitude">Latitude</label>
+                        <input className="w-full py-1 border rounded px-1" id="latitude" type="text" />
+
+                        <label className="block text-sm mt-2 font-bold" name="longtitude">Longtitude</label>
+                        <input className="w-full py-1 border rounded px-1" id="longtitude" type="text" />
+
+                        <button className="rounded bg-sky-600 hover:bg-sky-400 py-1 px-5 text-white text-center font-overpass mt-2" type="submit">UPDATE</button>
+                        </form>
+                    </div>
+
+                    <div className="rounded-t w-24 bg-sky-600 text-white text-center font-overpass p-1 px-2">RETRIEVE</div>
+                    <div className="border rounded-b p-2 font-overpass">
+                        <form>
+                        <label className="block text-sm font-bold" name="name">Name</label>
+                        <input className="w-full py-1 border rounded px-1" id="name" type="text" />
+
+                        <label className="block text-sm mt-2 font-bold" name="latitude">Latitude</label>
+                        <input className="w-full py-1 border rounded px-1" id="latitude" type="text" />
+
+                        <label className="block text-sm mt-2 font-bold" name="longtitude">Longtitude</label>
+                        <input className="w-full py-1 border rounded px-1" id="longtitude" type="text" />
+
+                        <button className="rounded bg-sky-600 hover:bg-sky-400 py-1 px-3 text-white text-center font-overpass mt-2" type="submit">RETRIEVE</button>
+                        </form>
+                    </div>
+
+                    <div className="rounded-t w-24 bg-sky-600 text-white text-center font-overpass p-1 px-2 mt-3">DELETE</div>
+                    <div className="border rounded-b p-2 font-overpass">
+                        <form>
+                        <label className="block text-sm font-bold" name="name">Name</label>
+                        <input className="w-full py-1 border rounded px-1" id="name" type="text" />
+
+                        <label className="block text-sm mt-2 font-bold" name="latitude">Latitude</label>
+                        <input className="w-full py-1 border rounded px-1" id="latitude" type="text" />
+
+                        <label className="block text-sm mt-2 font-bold" name="longtitude">Longtitude</label>
+                        <input className="w-full py-1 border rounded px-1" id="longtitude" type="text" />
+
+                        <button className="rounded bg-sky-600 hover:bg-sky-400 py-1 px-5 text-white text-center font-overpass mt-2" type="submit">DELETE</button>
+                        </form>
+                    </div>
+
+                    </div>
+
+                    <table className="table-fixed border-collapse border mx-auto w-full ">
+                        <thead>
+                            <tr className="bg-slate-100 text-slate-500">
+                                <th>Name</th>
+                                <th>Latitude</th>
+                                <th>Longtitude</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="text-slate-700">
+                                <th>01</th>
+                                <th>02</th>
+                                <th>03</th>
+                            </tr>
+                            <tr className="text-slate-700 border">
+                                <th>01</th>
+                                <th>02</th>
+                                <th>03</th>
+                            </tr>
+                        </tbody>
+
+                    </table>
+                    </div>
+                    </Collapse>
+                </div>
+            );
+        }
+    }
+
+    class UserData extends React.Component {
+        state = {
+            collapseOpen: false,
+          };
+
+        onCollapse = () => {
+            this.setState(prevState => ({
+              collapseOpen: !prevState.collapseOpen,
+            }));
+        };
+       
+        render(){
+            return(
+                <div className="m-3" id="user">
+                    <div className="rounded bg-gradient-to-r to-indigo-500 from-cyan-300 hover:from-purple-500 hover:to-rose-500 w-full p-0.5 opacity-80 font-overpass text-xl font-bold">
+                    <div className="rounded w-full bg-white p-2 h-full opacity-80 text-center" onClick={this.onCollapse}>User Data</div>
+                    </div>
+
+                    <Collapse isOpened= {this.state.collapseOpen}>
+                    <div className="rounded w-full p-2 border-2 border-indigo-100 mt-2 mb-5">
+
+                    <div className="columns-4 mb-3 mx-1 mt-1">  
+
+                    <div>
+                    <div className="rounded-t w-24 bg-sky-600 text-white text-center font-overpass p-1 px-2">CREATE</div>
+                   
+                    <div className="border rounded-b p-2 font-overpass">
+                        <form>
+                        <label className="block text-sm font-bold" name="name">Username</label>
+                        <input className="w-full py-1 border rounded px-1" id="name" type="text" />
+
+                        <button className="rounded bg-sky-600 hover:bg-sky-400 py-1 px-5 text-white text-center font-overpass mt-2" type="submit">CREATE</button>
+                        </form>
+                    </div>
+
+                    </div>
+
+                    <div>
+
+                    <div className="rounded-t w-24 bg-sky-600 text-white text-center font-overpass p-1 px-2 mt-3">UPDATE</div>
+                    
+                    <div className="border rounded-b p-2 font-overpass">
+                        <form>
+                        <label className="block text-sm font-bold" name="name">Username</label>
+                        <input className="w-full py-1 border rounded px-1" id="name" type="text" />
+
+
+                        <button className="rounded bg-sky-600 hover:bg-sky-400 py-1 px-5 text-white text-center font-overpass mt-2" type="submit">UPDATE</button>
+                        </form>
+                    </div>
+
+                    </div>
+
+                    <div>
+                    <div className="rounded-t w-24 bg-sky-600 text-white text-center font-overpass p-1 px-2">RETRIEVE</div>
+                    <div className="border rounded-b p-2 font-overpass">
+                        <form>
+                        <label className="block text-sm font-bold" name="name">Username</label>
+                        <input className="w-full py-1 border rounded px-1" id="name" type="text" />
+
+                        <button className="rounded bg-sky-600 hover:bg-sky-400 py-1 px-3 text-white text-center font-overpass mt-2" type="submit">RETRIEVE</button>
+                        </form>
+                    </div>
+                    </div>
+
+                    <div>
+
+                    <div className="rounded-t w-24 bg-sky-600 text-white text-center font-overpass p-1 px-2 mt-3">DELETE</div>
+                    <div className="border rounded-b p-2 font-overpass">
+                        <form>
+                        <label className="block text-sm font-bold" name="name">Username</label>
+                        <input className="w-full py-1 border rounded px-1" id="name" type="text" />
+
+                        <button className="rounded bg-sky-600 hover:bg-sky-400 py-1 px-5 text-white text-center font-overpass mt-2" type="submit">DELETE</button>
+                        </form>
+                    </div>
+                    </div>
+                    </div>
+
+                    <table className="table-fixed border-collapse border mx-auto w-full ">
+                        <thead>
+                            <tr className="bg-slate-100 text-slate-500">
+                                <th>Username</th>
+                                <th>Password</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="text-slate-700">
+                                <th>01</th>
+                                <th>02</th>
+                            </tr>
+                            <tr className="text-slate-700 border">
+                                <th>01</th>
+                                <th>02</th>
+                            </tr>
+                        </tbody>
+
+                    </table>
+                    </div>
+                    </Collapse>
+                </div>
+            );
+        }
+    }
+
+
+export default AdminHome;
+    
+
+
+/*
+1. Request updated data, i.e., reload from the online dataset, without affecting data which
+does not come from API (e.g., user comments within your app)
+2. CRUD stored location data (latlong and name) in the local database
+3. CRUD user data (username and password only) in the local database
+4. Log out as admin
+*/
