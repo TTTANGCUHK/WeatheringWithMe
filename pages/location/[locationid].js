094c72d3a55e757b3123665e2c44751de1e01afd
ken56789 <kenchan971971@gmail.com>
2022-05-13 15:32:29 +0800

    button

--- pages/location/[locationid].js
+++ pages/location/[locationid].js
@@ -118,7 +118,11 @@ function LocationPage() {
           </tbody>
         </table>
       </div>
-      <button />
+      <a href="../..">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          Back to home page
+        </button>
+      </a>
     </>
   );
 }
