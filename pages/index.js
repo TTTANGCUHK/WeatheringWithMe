a7bcdeb623d380b0d20a17de7bbf9ea185afd7dd
ken56789 <kenchan971971@gmail.com>
2022-05-13 20:04:06 +0800

    css

--- pages/index.js
+++ pages/index.js
@@ -53,12 +53,7 @@ function HomePage() {
     <>
       <Map locations={locations} />
       <Table locations={locations} />
-      <button
-        className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-        onClick={() => {
-          router.push("/favlocation/" + user._id);
-        }}
-      >
+      <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         View Your Favourite Location
       </button>
     </>
