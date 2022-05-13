6186775c7076fa502f2b809bef86c5db3ba22d72
trashiestcan <kingkai.chan@gmail.com>
2022-05-14 02:36:17 +0800

    Comment show username

--- component/CommentBox.js
+++ component/CommentBox.js
@@ -5,12 +5,25 @@ import axios from "axios"
 export default function CommentBox({ locName, uid }) {
   const [comments, setComments] = useState([])
   const [reload, setReload] = useState(0)
+  const [usernames, setUsernames] = useState([])
   useEffect(() => {
     axios.post('/api/comment', { action: 'get', payload: locName }).then(res => {
       console.log(res.data.comments)
       setComments(res.data.comments)
     })
   }, [locName, reload])
+
+  useEffect(() => {
+    setUsernames(comments.map((comment) => {
+      let username
+      getNameById(comment.uid).then(username => username)
+    }))
+    console.log(comments.map((comment) => {
+      let username
+      getNameById(comment.uid).then(name => username = name)
+      return username
+    }))
+  }, [comments])
 
   function handleComment(e) {
     e.preventDefault()
@@ -28,12 +41,20 @@ export default function CommentBox({ locName, uid }) {
     }
   }
 
+  async function getNameById(uid) {
+    let res = await axios.post('/api/user/retrieveName', { uid: uid })
+    return res.data.msg.username
+  }
+
   return (
     <div>
       {comments.map((comment, idx) => {
         return (
           <Card variant="outlined" key={idx}>
             <CardContent>
+              <Typography variant="body">
+                {usernames[idx]}
+              </Typography>
               <Typography variant="body">
                 {comment.body}
               </Typography>
---
+++ pages/api/user/retrieveName.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+import connectToMongoDB from '../../../backend/mongo/mongoDB'
+import userModel from '../../../backend/dbSchema/userSchema'
+import APICHECK from '../APICHECK'
+
+export default async function handler(req, res) {
+  let err
+  if (err = APICHECK.POST_only(req), err)
+    return res.status(err.httpStatus).json(err.apiRes)
+
+  if (err = APICHECK.PARAMS_REQUIRE(req, ['uid']), err)
+    return res.status(err.httpStatus).json(err.apiRes)
+
+  await connectToMongoDB()
+  const db = mongoose.connection
+  const User = userModel
+  let uid = req.body.uid
+  if (uid === undefined)
+    return res.status(400).json({ status: 'error', msg: 'Some field is empty' })
+
+  User.findOne({ _id: uid }, (err, user) => {
+    if (err) return res.status(400).json({ status: 'error', msg: err })
+    if (user == null) {
+      return res.status(404).json({ status: '404', msg: 'Username does not exist' })
+    }
+    const username = user.username
+    const userPW = user.password
+    const userSALT = user.salt
+    return res.status(200).json({ status: '200', msg: { username: username } })
+
+  });
+
+}
