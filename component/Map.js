1e8b7a91badc63dbbd3b88156e802a9629adb692
ken56789 <kenchan971971@gmail.com>
2022-05-13 19:48:49 +0800

    map

--- component/Map.js
+++ component/Map.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
-function Map({locations}) {
-  const router = useRouter()
+function Map({ locations }) {
+  const router = useRouter();
   const [libraries] = useState(["places"]);
   const { isLoaded, loadError } = useLoadScript({
-      googleMapsApiKey:"AIzaSyAw6wxQHqtInbxZz-O6kdq0JOs_DGNkAA4",
-      libraries,
+    googleMapsApiKey: "AIzaSyAw6wxQHqtInbxZz-O6kdq0JOs_DGNkAA4",
+    libraries,
   });
 
   const mapContainerStyle = {
-      width: "70%",
-      height: "700px",
-    };
-    
+    width: "70%",
+    height: "700px",
+  };
+
   const [center, setCenter] = useState({
-      lat: 17.5806386,
-      lng: 54.2365876,
+    lat: 17.5806386,
+    lng: 54.2365876,
   });
 
   const options = {
@@ -29,23 +29,30 @@ function Map({locations}) {
   if (!isLoaded) return "";
 
   return (
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      zoom={3}
-      center={center}
-      options={options}
-    >
-      {
-        locations.map((location, index) => (
-          <Marker
-            key={location._id}
-            position={ {lat: location.locData.latitude, lng: location.locData.longitude }}
-            onClick={() => {router.push("/location/" + location._id)}}
-          />
-        ))
-      }
-    </GoogleMap>
-    )
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={3}
+          center={center}
+          options={options}
+        >
+          {locations.map((location, index) => (
+            <Marker
+              key={location._id}
+              position={{
+                lat: location.locData.latitude,
+                lng: location.locData.longitude,
+              }}
+              onClick={() => {
+                router.push("/location/" + location._id);
+              }}
+            />
+          ))}
+        </GoogleMap>
+      </div>
+    </>
+  );
 }
 
 export default Map;
